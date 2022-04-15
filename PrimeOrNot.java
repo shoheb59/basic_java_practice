49606b6f4250eb4fccb686a86d04948d424ca440
Md. Hasnat Shoheb <57152392+shoheb59@users.noreply.github.com>
2022-04-15 14:12:25 +0600

    Update PrimeOrNot.java

--- PrimeOrNot.java
+++ PrimeOrNot.java
@@ -7,23 +7,32 @@ public class PrimeOrNot {
         int num = scanner.nextInt();
         int prime =  primeNum(num);
         System.out.println(prime +" number is checked");
+        boolean flag = false;
 
     }
     static int primeNum(int x)
     {
-        for (int i = 2; i < x; i++)
+        boolean flag = false;
+        for (int i = 2; i < x/2; i++)
         {
             if (x%i == 0)
             {
                 System.out.println("Not a Prime Number");
+                flag = true;
                 break;
+
+
+
             }
-            else
-           {
-                System.out.println("Prime number");
-                break;
-            }
+
         }
+        if(flag == false)
+        {
+            System.out.println("Prime Number");
+        }
+
+
+
 
         return x;
 
